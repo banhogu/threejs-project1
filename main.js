690b44fc201d4336e2a50dd3b42a6f6c87d2af55
hojin <devbang5674@gmail.com>
2024-10-21 19:29:58 +0900

    feat: 포인터 적용 실습

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ const camera = new THREE.PerspectiveCamera(
 const floorGeometry = new THREE.PlaneGeometry(20, 20);
 const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.name = "floor";
 floor.rotation.x = -Math.PI / 2;
 floor.position.y = 0;
 floor.receiveShadow = true;
@@ -54,14 +55,14 @@ window.addEventListener("resize", () => {
 });
 
 const gltfLoader = new GLTFLoader();
-
 // gltfLoader를 이용해서 glb파일 모델링을 로드함
 const gltf = await gltfLoader.loadAsync("/dancer.glb");
-console.log(gltf);
 const character = gltf.scene; // 모델링의 골격과 옷
 const animationClips = gltf.animations; // 모델링의 애니메이션[]
 
 character.position.y = 0.8; // 높이 조정
+character.position.x = 1;
+character.position.z = 1;
 character.scale.set(0.01, 0.01, 0.01); // 크기 조정
 
 // character는 여러개의 부품 메시로 이루어져 있으므로, 각각의 메시에 대하여 그림자 설정을 모두 해주어야 정상적으로 그림자 효과를 낼 수 있다.
@@ -74,22 +75,13 @@ character.traverse((mesh) => {
 
 // 애니메이션 클립을 관리하는 믹서
 const mixer = new THREE.AnimationMixer(character);
+console.log("animationClips", animationClips);
 
 // 믹서에 animation 클립을 지정하고 재생
 const action = mixer.clipAction(animationClips[3]);
 
-// action.setLoop(THREE.LoopOnce); // 한번만 재생
-// action.setLoop(THREE.LoopRepeat);
 action.setLoop(THREE.LoopPingPong); // 처음 -> 끝 -> 처음 ... 무한 재생
-// action.setDuration(10); // 애니메이션 재생에 걸리는 속력 조정
-// action.setEffectiveTimeScale(2); // 액션의 재생 속력 비율(2일 경우, 2배가 빠름)
-// action.setEffectiveWeight(0.5); // 액션이 분명한 정도(수치가 낮을수록 모델이 춤을 대충 추는 느낌)
 action.play();
-
-// 5초 후 애니메이션 일시정지
-// setTimeout(() => {
-//   mixer.clipAction(animationClips[3]).paused = true;
-// }, 5000);
 
 scene.add(character);
 
@@ -115,12 +107,43 @@ const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.enableDamping = true; // 기본값: false, 카메라 시점을 돌릴 때, 바로 멈추는게 아닌, 좀 더 진행방향으로 이동 후 멈추는 효과 (update 메소드를 애니메이션 프레임 안에서 실행해야 정상적으로 동작함)
 orbitControls.dampingFactor = 0.03; // 기본값: 0.05, enableDamping이 true일 때, 값이 작아질수록 더욱 부드럽게 정지됨
 
+const newPosition = new THREE.Vector3(0, 1, 0); // 클릭된 3D 상의 좌표를 저장할 벡터
+const rayCaster = new THREE.Raycaster();
+
+renderer.domElement.addEventListener("pointerdown", (e) => {
+  // client좌표를 threejs 평면상의 좌표로 환산
+  const x = (e.clientX / window.innerWidth) * 2 - 1; // 화면 상 가장 왼쪽이 -1, 중심이 0, 오른쪽이 1이 되도록 환산
+  const y = -((e.clientY / window.innerHeight) * 2 - 1); // 화면 상 가장 위쪽이 1, 중심이 0, 아래쪽이 -1이 되도록 환산
+
+  rayCaster.setFromCamera(new THREE.Vector2(x, y), camera); // rayCaster에 환산된 좌표와 카메라를 넘겨줌
+  const intersects = rayCaster.intersectObjects(scene.children); // scene의 children 오브젝트들 중 현재 rayCaster가 관통한 오브젝트들을 받음
+  console.log(intersects);
+  const intersectFloor = intersects.find((i) => i.object.name === "floor"); // 관통한 오브젝트들 중 이름이 'floor'인 객체를 찾음
+  newPosition.copy(intersectFloor.point); // 캐릭터가 이동할 위치벡터인 newPosition에 현재 클릭된 위치 좌표로 설정함
+  newPosition.y = 1;
+});
+
 const clock = new THREE.Clock();
+const targetVector = new THREE.Vector3();
 const render = () => {
   orbitControls.update(); // OrbitControls의 enableDamping 효과를 적용하려면 애니메이션 루프 안에서 update 메소드를 실행해야함
-  // character.rotation.x += 0.01;
-  // character.rotation.y += 0.01;
-  // character.rotation.z += 0.01;
+
+  character.lookAt(newPosition); // 우선 캐릭터가 클릭된 위치를 바라보도록 설정
+  targetVector
+    .subVectors(newPosition, character.position)
+    .normalize()
+    .multiplyScalar(0.01); // 캐릭터가 목적지로 이동하기 위한 가이드 벡터( 목적지 - 캐릭터의 현재 위치 로 구한 벡터를 정규화(크기를 1로 만들기) 한 후, 0.01배의 크기로 줄임)
+
+  // 캐릭터가 목표한 위치로 도달하도록 계속해서 위치를 업데이트함
+  if (
+    Math.abs(character.position.x - newPosition.x) >= 1 ||
+    Math.abs(character.position.z - newPosition.z) >= 1
+  ) {
+    character.position.x += targetVector.x;
+    character.position.z += targetVector.z;
+    action.stop();
+  }
+  action.play();
 
   // 믹서 업데이트
   if (mixer) {
