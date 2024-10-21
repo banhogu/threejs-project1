6eb47ce78a543edfae000c6248a7cb4f37e7c7eb
hojin <devbang5674@gmail.com>
2024-10-21 19:26:09 +0900

    feat: 애니메이션 실습

--- main.js
+++ main.js
@@ -54,22 +54,43 @@ window.addEventListener("resize", () => {
 });
 
 const gltfLoader = new GLTFLoader();
-// gltfLoader.load("/dancer.glb", (data) => {
-//   console.log(data);
-//   scene.add(data.scene);
-// });
 
+// gltfLoader를 이용해서 glb파일 모델링을 로드함
 const gltf = await gltfLoader.loadAsync("/dancer.glb");
-console.log(gltf.scene);
-const character = gltf.scene;
-character.position.y = 0.8;
-character.scale.set(0.01, 0.01, 0.01);
+console.log(gltf);
+const character = gltf.scene; // 모델링의 골격과 옷
+const animationClips = gltf.animations; // 모델링의 애니메이션[]
+
+character.position.y = 0.8; // 높이 조정
+character.scale.set(0.01, 0.01, 0.01); // 크기 조정
+
+// character는 여러개의 부품 메시로 이루어져 있으므로, 각각의 메시에 대하여 그림자 설정을 모두 해주어야 정상적으로 그림자 효과를 낼 수 있다.
 character.traverse((mesh) => {
   if (mesh.isMesh) {
     mesh.castShadow = true;
     mesh.receiveShadow = true;
   }
 });
+
+// 애니메이션 클립을 관리하는 믹서
+const mixer = new THREE.AnimationMixer(character);
+
+// 믹서에 animation 클립을 지정하고 재생
+const action = mixer.clipAction(animationClips[3]);
+
+// action.setLoop(THREE.LoopOnce); // 한번만 재생
+// action.setLoop(THREE.LoopRepeat);
+action.setLoop(THREE.LoopPingPong); // 처음 -> 끝 -> 처음 ... 무한 재생
+// action.setDuration(10); // 애니메이션 재생에 걸리는 속력 조정
+// action.setEffectiveTimeScale(2); // 액션의 재생 속력 비율(2일 경우, 2배가 빠름)
+// action.setEffectiveWeight(0.5); // 액션이 분명한 정도(수치가 낮을수록 모델이 춤을 대충 추는 느낌)
+action.play();
+
+// 5초 후 애니메이션 일시정지
+// setTimeout(() => {
+//   mixer.clipAction(animationClips[3]).paused = true;
+// }, 5000);
+
 scene.add(character);
 
 camera.position.set(1, 2, 2);
@@ -94,9 +115,17 @@ const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.enableDamping = true; // 기본값: false, 카메라 시점을 돌릴 때, 바로 멈추는게 아닌, 좀 더 진행방향으로 이동 후 멈추는 효과 (update 메소드를 애니메이션 프레임 안에서 실행해야 정상적으로 동작함)
 orbitControls.dampingFactor = 0.03; // 기본값: 0.05, enableDamping이 true일 때, 값이 작아질수록 더욱 부드럽게 정지됨
 
+const clock = new THREE.Clock();
 const render = () => {
   orbitControls.update(); // OrbitControls의 enableDamping 효과를 적용하려면 애니메이션 루프 안에서 update 메소드를 실행해야함
+  // character.rotation.x += 0.01;
+  // character.rotation.y += 0.01;
+  // character.rotation.z += 0.01;
 
+  // 믹서 업데이트
+  if (mixer) {
+    mixer.update(clock.getDelta());
+  }
   renderer.render(scene, camera);
   // 애니메이션 프레임 루프에서 재귀적으로 계속 렌더함수를 호출하여 애니메이션 처리
   requestAnimationFrame(render);
